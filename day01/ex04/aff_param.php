710fdb8cc609a8f61b695e6d516ac35188e881cf
Saad Karkal <sakarkal@e1r11p2.1337.ma>
2022-12-20 20:07:58 +0100

    42

--- day01/ex04/aff_param.php
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/php
-<?php
-$arg = 1;
-foreach ($argv as $elem)
-{
-	if ($arg++ > 1)
-		echo "$elem"."\n";
-}
-?>
--- day05/ex19/ex19.sql
+++
@@ -1,2 +0,0 @@
-SELECT TIMESTAMPDIFF(DAY, MIN(`date`), MAX(`date`)) AS 'uptime'
-FROM `member_history`;
